a9a4a34ad9ee06e18adf060a354968872c693c5d
Gladrat <3368584+Gladrat@users.noreply.github.com>
2026-05-17 10:02:07 +0200

    add cors support

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 import requests
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 NOTES_URL = "https://raw.githubusercontent.com/Gladrat/notes_M1CS/main/notes.json"
 
